eca600786a4fa3af77a461015529f6a6be9c2258
lyc2760008 <44538498+lyc2760008@users.noreply.github.com>
2019-04-05 16:11:30 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# web2
+# COMP 3512 (Winter 2019)
+### Assignment #2: PHP (plus JS)
+
+**Please view `COMP3512 Assignment 2.pdf` for instructions**
+
+  
---
+++ clear_fav.php
@@ -0,0 +1,7 @@
+<?php
+	session_start();
+	unset($_SESSION['favs']);
+	$_SESSION['message'] = 'Cart cleared successfully';
+	header('location: index.php');
+	//Remove all favorites from the table and redirect to home page
+?>
